f1a054235bc59e23f15a1787fbbbb41b27c2052c
Roberta Lima <roberta.lima@printi.com.br>
2023-11-27 19:16:58 -0300

    Create delete players feature

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,13 +1,20 @@
 import { PropTypes } from "prop-types";
 
-const Player = ({ name, score }) => {
+const Player = ({ name, score, onDeletePlayer }) => {
   return (
     <div className="col-span-4 grid grid-cols-4 border-t-2">
       <span className="col-span-3 bg-slate-800 py-5 pl-8 text-xl font-semibold text-slate-50">
+        <button
+          title="Delete player"
+          className="mr-4 rounded-full border-2 px-2 text-lg transition hover:border-red-500 hover:text-red-500"
+          onClick={() => onDeletePlayer(name)}
+        >
+          X
+        </button>
         {name}
       </span>
       <div className="flex items-center justify-center bg-slate-800 text-slate-50">
-        <button className="py-5text-lg h-full bg-red-500 px-6 font-bold transition hover:opacity-90">
+        <button className="h-full bg-red-500 px-6 py-5 text-lg font-bold transition hover:opacity-90">
           -
         </button>
         <span className="grow text-center text-2xl">{score}</span>
@@ -22,6 +29,7 @@ const Player = ({ name, score }) => {
 Player.propTypes = {
   name: PropTypes.string.isRequired,
   score: PropTypes.number.isRequired,
+  onDeletePlayer: PropTypes.func.isRequired,
 };
 
 export default Player;
--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -3,22 +3,55 @@ import StopWatch from "./StopWatch";
 import Player from "./Player";
 import PlayerInput from "./PlayerInput";
 import { useCallback, useState } from "react";
+import ErrorModal from "./ErrorModal";
 
 const Scoreboard = () => {
   const [players, setPlayers] = useState([]);
+  const [showErrorModal, setShowErrorModal] = useState(false);
 
-  const addPlayers = useCallback((newPlayer) => {
-    setPlayers((oldPlayers) => [...oldPlayers, newPlayer]);
-  }, []);
+  const isPlayerNameRepeated = useCallback(
+    (playerName) => !!players.filter((p) => playerName === p.name).length,
+    [players],
+  );
+
+  const addPlayers = useCallback(
+    (newPlayer) => {
+      if (isPlayerNameRepeated(newPlayer.name)) {
+        setShowErrorModal(true);
+        return;
+      }
+
+      setPlayers((oldPlayers) => [...oldPlayers, newPlayer]);
+    },
+    [isPlayerNameRepeated],
+  );
+
+  const deletePlayers = useCallback(
+    (playerName) => {
+      const updatedPlayers = players.filter((p) => p.name !== playerName);
+
+      setPlayers(updatedPlayers);
+    },
+    [players],
+  );
 
   return (
     <div className="m-auto grid w-fit grid-cols-4 uppercase tracking-wider">
       <Header />
       <StopWatch />
       {players.map(({ name, score }) => (
-        <Player key={name} name={name} score={score} />
+        <Player
+          key={name}
+          name={name}
+          score={score}
+          onDeletePlayer={deletePlayers}
+        />
       ))}
       <PlayerInput onAddPlayer={addPlayers} />
+
+      {showErrorModal && (
+        <ErrorModal onClose={() => setShowErrorModal(false)} />
+      )}
     </div>
   );
 };
