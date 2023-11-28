3fdb5f310d972284196162a5ba722c46b2afa4e0
Roberta Lima <roberta.lima@printi.com.br>
2023-11-28 14:07:06 -0300

    Add animation to error modal

--- src/components/ErrorModal.jsx
+++ src/components/ErrorModal.jsx
@@ -1,8 +1,29 @@
 import { PropTypes } from "prop-types";
+import { useCallback, useEffect, useState } from "react";
 
 const ErrorModal = ({ onClose }) => {
+  const [hideModal, setHideModal] = useState(false);
+
+  const closeModal = useCallback(() => {
+    setHideModal(false);
+    onClose();
+  }, [onClose]);
+
+  useEffect(() => {
+    let id;
+    if (hideModal) {
+      id = setInterval(closeModal, 1000);
+    }
+
+    return () => clearInterval(id);
+  }, [hideModal, closeModal]);
+
   return (
-    <div className="fixed self-center justify-self-center rounded bg-slate-50 shadow-md">
+    <div
+      className={`${
+        hideModal ? "animate-hideModal" : "animate-showModal"
+      } fixed self-center justify-self-center rounded bg-slate-50 shadow-md`}
+    >
       <div className="flex flex-col items-center p-4">
         <img
           src="https://100dayscss.com/codepen/alert.png"
@@ -16,7 +37,7 @@ const ErrorModal = ({ onClose }) => {
       <div>
         <button
           className="w-full rounded-b bg-red-500 py-2 uppercase text-slate-50 transition hover:opacity-90"
-          onClick={onClose}
+          onClick={() => setHideModal(true)}
         >
           Dismiss
         </button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,27 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      animation: {
+        showModal: "showModal .7s ease-in-out",
+        hideModal: "hideModal .6s ease-in-out both",
+      },
       fontFamily: {
         sans: ['"Zen Kaku Gothic Antique"', "system-ui"],
         serif: ["ui-serif", "Georgia"],
         mono: ["ui-monospace", "SFMono-Regular"],
+      },
+      keyframes: {
+        showModal: {
+          "0%": { transform: "scale(0)" },
+          "60%": { transform: "scale(1.1)" },
+          "80%": { transform: "scale(.95)" },
+          "100%": { transform: "scale(1)" },
+        },
+        hideModal: {
+          "0%": { transform: "scale(1)" },
+          "20%": { transform: "scale(1.1)" },
+          "100%": { transform: "scale(0)" },
+        },
       },
     },
   },
